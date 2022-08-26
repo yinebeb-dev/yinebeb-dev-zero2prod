aefdd85dab91e0705dc35074a47238ee14f589e1
yinebeb atel <yinebeb@tribecap.co>
2022-08-26 14:17:21 -0700

    refactor routes

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,13 @@
+pub mod routes;
 pub mod configuration;
 
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpResponse, HttpServer};
-use serde::{Deserialize, Serialize};
+use actix_web::{web, App, HttpServer};
 use std::net::TcpListener;
 
-async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
+use routes::health_check;
+use routes::subscribe;
 
-#[derive(Deserialize)]
-struct FormData {
-    email: String,
-    name: String,
-}
-
-async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
